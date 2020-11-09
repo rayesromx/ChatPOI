b98ee32b8e84a8cad1a648ccd72078137f70c6b3
Ray Espinosa <ray.esro@gmail.com>
2020-11-09 17:09:36 -0600

    Se agrega la carcasa de la pantalla de login y registro

---
+++ app/src/main/java/fcfm/lmad/poi/ChatPoi/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package fcfm.lmad.poi.ChatPoi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
